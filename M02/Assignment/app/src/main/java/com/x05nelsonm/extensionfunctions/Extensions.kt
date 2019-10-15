cf9cf085d85a79f06cf2457f421b1ce5f27e9cdd
Matthew Nelson <05nelsonm@protonmail.com>
2019-10-15 17:10:50 -0400

    extended the notification builder

---
+++ M02/Assignment/app/src/main/java/com/x05nelsonm/extensionfunctions/Extensions.kt
@@ -0,0 +1,46 @@
+package com.x05nelsonm.extensionfunctions
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+
+fun Context.showNotification(
+    notificationId: Int,
+    channelId: String,
+    contentTitle: String,
+    contentText: String,
+    notificationManagerImportance: Int,
+    colorized: Boolean,
+    resourceColor: Int
+    ) {
+    
+    
+    val channelId = "${this.packageName}.$channelId"
+    val notificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val name = channelId
+        val description = "Notification channel"
+
+        val channel = NotificationChannel(channelId, name, notificationManagerImportance)
+        channel.description = description
+
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    val notificationBuilder = NotificationCompat.Builder(this, channelId)
+        .setPriority(notificationManagerImportance)
+        .setSmallIcon(android.R.drawable.alert_dark_frame)
+        .setContentTitle(contentTitle)
+        .setContentText(contentText)
+        .setAutoCancel(true)
+    
+    if (colorized) {
+        notificationBuilder
+            .setColorized(true)
+            .setColor(resourceColor)
+    }
+    notificationManager.notify(notificationId, notificationBuilder.build())
+}
--- M02/Assignment/app/src/main/java/com/x05nelsonm/extensionfunctions/MainActivity.kt
+++ M02/Assignment/app/src/main/java/com/x05nelsonm/extensionfunctions/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.x05nelsonm.extensionfunctions
 
+import android.app.NotificationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_notification.setOnClickListener {
+            showNotification(
+                0,
+                "",
+                "",
+                "This is my Notification",
+                NotificationManager.IMPORTANCE_HIGH,
+                true,
+                ContextCompat.getColor(this, R.color.colorPrimary)
+            )
+        }
     }
 }
