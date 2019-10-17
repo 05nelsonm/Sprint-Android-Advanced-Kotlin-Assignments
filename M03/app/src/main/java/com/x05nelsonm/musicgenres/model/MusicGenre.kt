ada6d01360989c087c96b03ab4f3f0220b905944
Matthew Nelson <05nelsonm@protonmail.com>
2019-10-16 20:22:05 -0400

    updated model in accordance with assignment requirements

--- M03/app/src/main/java/com/x05nelsonm/musicgenres/model/MusicGenre.kt
+++ M03/app/src/main/java/com/x05nelsonm/musicgenres/model/MusicGenre.kt
@@ -1,3 +1,9 @@
 package com.x05nelsonm.musicgenres.model
 
-class MusicGenre (val name: String)
+sealed class MusicGenre (val name: String)
+
+class Rock (name: String): MusicGenre(name)
+class HeavyMetal (name: String): MusicGenre(name)
+class MetalCore (name: String): MusicGenre(name)
+class TechnoCore (name: String): MusicGenre(name)
+class MichaelJackson (name: String): MusicGenre(name)
